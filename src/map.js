a2179b5f67ec93fb5bd10a747d7b46361b5b1c59
Andrew Fischer <afischer15@mac.com>
2020-09-20 12:41:46 -0400

    move to d3

--- src/map.js
+++ src/map.js
@@ -1,106 +1,108 @@
 import pubGeoJSON from '../data/pubDB.json';
-import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+import us from '../data/states-albers-10m.json'
+import * as d3 from 'd3';
+import * as topojson from 'topojson-client';
+
 
 export default () => {
-  console.log(pubGeoJSON);
-  // restricted token
-  mapboxgl.accessToken = 'pk.eyJ1IjoiYWZpc2NoZXIxNSIsImEiOiJja2YxYmVveTQwcmM5MnhsbmFpZzN0cG12In0.ODgJerfBfyVURmzQhHwR_A';
-  const map = new mapboxgl.Map({
-    container: 'map',
-    // style: 'mapbox://styles/mapbox/light-v10', // stylesheet location
-    style: 'mapbox://styles/afischer15/ckf1bkh3y3im619mgu9qlsxu7',
-    // center: [-98, 39.5], // starting position [lng, lat]
-    center: [0, 0],
-    zoom: 4 // starting zoom
-  })
+  // create SVG
+  var svg = d3.select("#map")
+    .append("svg")
+    .attr('viewBox', '0 0 975 619');;
 
-  map.on('load', function () {
+  // add tooltip and handlers
+  const tooltip = d3.select("#map")
+    .append("div")
+    .style("opacity", 0)
+    .attr("class", "tooltip")
 
-    // for (const pub of publications) {
-    //   const { name, SchoolUrl } = pub;
-    //   map.loadImage(`https://logo.clearbit.com/${SchoolUrl}`, (err, img) => {
-    //     map.addImage(name, img);
-    //   })
-    // }
-    console.log('adding');
+  const handleMouseOver = (d) => {
+    const data = JSON.parse(d.target.dataset.info);
+    tooltip
+      .style("opacity", 1)
+      .html(getTooltipHtml(data))
 
-    map.addSource('pubData', { type: 'geojson', data: pubGeoJSON });
-    console.log('added');
-    // // Add a symbol layer
-    map.addLayer({
-      'id': 'pubDataPoints',
-      // 'type': 'image',
-      type: 'circle',
-      source: 'pubData',
-      layout: {
-        // 'icon-image': 'school-15',
-        // 'icon-image': ['get', 'name'],
-        // 'icon-size': 0.2,
-        // get the title name from the source's "title" property
-        // 'text-field': [
-        //   'format',
-        //   ['get', 'name'],
-        //   { 'font-scale': 0.8 },
-        //   '\n',
-        //   {},
-        //   ['upcase', ['get', 'university']],
-        //   { 'font-scale': 0.5 }
-        // ],
-        // 'text-font': [
-        //   'Open Sans Semibold',
-        //   'Arial Unicode MS Bold'
-        // ],
-        // 'text-offset': [0, .3],
-        // 'text-anchor': 'top',
-        // 'icon-ignore-placement': true,
-        // 'text-ignore-placement': true
-      },
-      paint: {
-        'circle-color': '#000000',
-        'circle-opacity': 0.5,
-        "circle-radius": 3
-      }
-    });
+  }
 
-    var popup = new mapboxgl.Popup({
-      closeButton: false,
-      closeOnClick: false,
-      offset: 3
-    });
+  const handleMouseMove = d => {
+    const { clientX, clientY } = d;
+    tooltip
+      .style("left", clientX + 10 + "px")
+      .style("top", clientY + 10 + "px")
+  }
 
-    map.on('mousemove', 'pubDataPoints', function (e) {
-      // Change the cursor style as a UI indicator.
-      map.getCanvas().style.cursor = 'pointer';
-      var coordinates = e.features[0].geometry.coordinates.slice();
-      var { properties } = e.features[0];
+  const handleMouseOut = d => {
+    tooltip
+      .style("opacity", 0)
+  }
 
-      const description = `
-      <h3><em>${properties.name}</em></h3>
-      <div>${properties.university} ${properties.school ? `(${properties.school})` : ''}</div>
+  const getTooltipHtml = data => {
+    const { name, university, school, SchoolUrl } = data;
+    return `
+      <div class="pub-title">
+        <img src="https://logo.clearbit.com/${SchoolUrl}" width="32px" height="32px" />
+        <div>
+          <h3><em>${name}</em></h3>
+          ${university} ${school ? `(${school})` : ''}
+        </div>
+      </div>
+      <p> Other info will go here! </p>
       `
+  }
 
-      // Ensure that if the map is zoomed out such that multiple
-      // copies of the feature are visible, the popup appears
-      // over the copy being pointed to.
-      while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-        coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-      }
+  // top level group for zooming
+  const items = svg.append("g");
 
-      // Populate the popup and set its coordinates
-      // based on the feature found.
-      popup
-        .setLngLat(coordinates)
-        .setHTML(description)
-        .addTo(map);
-    });
+  // add state paths
+  const stateData = topojson.feature(us, us.objects.states).features;
+  const path = d3.geoPath();
 
-    map.on('mouseleave', 'pubDataPoints', function () {
-      map.getCanvas().style.cursor = '';
-      popup.remove();
-    });
+  items
+    .append('g')
+    .selectAll('path')
+    .data(stateData)
+    .enter()
+    .append('path')
+    .attr('fill', '#d4d4d4')
+    .attr('stroke', '#fff')
+    .attr('d', path)
 
 
-  });
+  // reproject points onto the map
+  const pointRadius = 4;
+  const projection = d3.geoAlbersUsa().scale(1300).translate([487.5, 305])
+
+  items.selectAll("circle")
+    .data(pubGeoJSON.features)
+    .enter()
+    .append("circle")
+    .attr("cx", d => projection(d.geometry.coordinates)[0])
+    .attr("cy", d => projection(d.geometry.coordinates)[1])
+    .attr("r", pointRadius)
+    .attr("fill", "#888")
+    .attr("opacity", ".75")
+    .attr("stroke", "#eee")
+    .attr("data-info", d => JSON.stringify(d.properties))
+    .on('mouseover', handleMouseOver)
+    .on('mousemove', handleMouseMove)
+    .on('mouseout', handleMouseOut);
+
+  // zoom functionality
+  function handleZoom(event) {
+    const { transform } = event;
+    items.attr("transform", transform);
+    items.attr("stroke-width", 1 / transform.k);
+    items
+      .selectAll('circle')
+      .attr("r", Math.max(pointRadius / 30, pointRadius / transform.k));
+  }
+
+  const zoom = d3.zoom()
+    .scaleExtent([1, 50])
+    .translateExtent([[0, 0], [975, 619]])
+    .on("zoom", handleZoom);
+
+  svg.call(zoom);
+
 
 }
-
